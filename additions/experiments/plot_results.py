2b80e228c883efe098e38da849d065ce257e9041
Andrea Soprani <andrea.soprani96@gmail.com>
2020-03-24 16:00:56 +0100

    3-room linear re-done

--- additions/experiments/plot_results.py
+++ additions/experiments/plot_results.py
@@ -10,8 +10,8 @@ import glob
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--path", default="results/three-room-gw/sin/lambda=1.0/")
-parser.add_argument("--title", default="three-room-gw - sin (lambda = 1.0)")
+parser.add_argument("--path", default="results/three-room-gw/linear/")
+parser.add_argument("--title", default="three-room-gw - linear")
 
 args = parser.parse_args()
 path = str(args.path)
