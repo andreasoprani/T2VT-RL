19c4f93cf770aa1b6d122d302553745791e7e82c
Andrea Soprani <andrea.soprani96@gmail.com>
2020-03-27 16:22:27 +0100

    trading

--- additions/experiments/trading/run.py
+++ additions/experiments/trading/run.py
@@ -9,21 +9,15 @@ path = os.path.dirname(os.path.realpath(__file__)) + "/" # path to this folder
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--gen_samples", default=True)
-parser.add_argument("--max_iter_gen", default=15000000)
-parser.add_argument("--l1", default=64)
-parser.add_argument("--l2", default=0)
 parser.add_argument("--mgvt_1", default=True)
-parser.add_argument("--mgvt_3", default=True)
+parser.add_argument("--mgvt_3", default=False)
 parser.add_argument("--rtde_1", default=True)
-parser.add_argument("--rtde_3", default=True)
-parser.add_argument("--n_runs", default=5)
-parser.add_argument("--max_iter", default=10000)
+parser.add_argument("--rtde_3", default=False)
+parser.add_argument("--n_runs", default=1)
+parser.add_argument("--max_iter", default=10000000)
 
 args = parser.parse_args()
 gen = bool(args.gen_samples)
-max_iter_gen = int(args.max_iter_gen)
-l1 = int(args.l1)
-l2 = int(args.l2)
 mgvt_1 = bool(args.mgvt_1)
 mgvt_3 = bool(args.mgvt_3)
 rtde_1 = bool(args.rtde_1)
@@ -39,13 +33,14 @@ tasks = {
         }
 
 gen_samples = "gen_samples.py"
-timesteps = [2014, 2015, 2016, 2017]
+timesteps = [#2014, 
+             2015, 
+             2016, 
+             2017]
 
 if gen:
     for t in timesteps:
         f = gen_samples
-        f += " --max_iter=" + str(max_iter_gen)
-        f += " --l1=" + str(l1) + " --l2=" + str(l2)
         f += " --just_one_timestep=" + str(t)
         print(f + " - " + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
         subprocess.call(python + " " + path + f, shell=True)
@@ -55,7 +50,6 @@ for k, v in tasks.items():
         continue
     f = k
     f += " --max_iter=" + str(max_iter)
-    f += " --l1=" + str(l1) + " --l2=" + str(l2)
     f += " --n_runs=" + str(n_runs)
     print(f + " - " + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
     subprocess.call(python + " " + path + f, shell=True)
