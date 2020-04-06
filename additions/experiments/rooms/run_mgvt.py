659e13a349de2d594641ae4f65ace41d15b20021
Andrea Soprani <andrea.soprani96@gmail.com>
2020-04-06 10:30:02 +0200

    three-room linear 50 seeds

--- additions/experiments/rooms/run_mgvt.py
+++ additions/experiments/rooms/run_mgvt.py
@@ -14,6 +14,8 @@ from misc import utils
 import argparse
 from joblib import Parallel, delayed
 import datetime
+import glob
+import errno
 
 # Global parameters
 render = False
@@ -43,9 +45,10 @@ parser.add_argument("--cholesky_clip", default=0.0001)
 # Door at -1 means random positions over all runs
 parser.add_argument("--n_basis", default=11)
 parser.add_argument("--n_jobs", default=1)
-parser.add_argument("--n_runs", default=20)
+parser.add_argument("--n_runs", default=50)
 parser.add_argument("--source_file", default=path + "/sources")
 parser.add_argument("--tasks_file", default=path + "/tasks")
+parser.add_argument("--load_results", default = False) # load previously found results and extend them
 parser.add_argument("--eta", default=1e-6)  # learning rate for
 parser.add_argument("--eps", default=0.001)  # precision for the initial posterior approximation and upperbound tighting
 parser.add_argument("--bandwidth", default=.00001)  # Bandwidth for the Kernel Estimator
@@ -80,6 +83,7 @@ n_jobs = int(args.n_jobs)
 n_runs = int(args.n_runs)
 source_file = str(args.source_file)
 tasks_file = str(args.tasks_file)
+load_results = bool(args.load_results)
 eps = float(args.eps)
 eta = float(args.eta)
 post_components = int(args.post_components)
@@ -89,13 +93,21 @@ max_iter_ukl = int(args.max_iter_ukl)
 file_path = "results/" + env + "/" + experiment_type + "/"
 if not os.path.exists(file_path):
     os.mkdir(file_path)
-file_name = "mgvt_" + str(post_components) + "c_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+if load_results:
+    file_name = "mgvt_" + str(post_components) + "c_"
+    f = file_path + file_name + "*.pkl"
+    fs = glob.glob(f)
+    if len(fs) == 0:
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), f)
+    file_name = fs[0][:-4]
+else:
+    file_name = file_path + "mgvt_" + str(post_components) + "c_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 source_file += "-" + experiment_type
 source_file += "-2r" if env == "two-room-gw" else ("-3r" if env == "three-room-gw" else "")
 tasks_file += "-" + experiment_type
 tasks_file += "-2r" if env == "two-room-gw" else ("-3r" if env == "three-room-gw" else "")
-
 
 # Seed to get reproducible results
 seed = 1
@@ -149,14 +161,29 @@ def run(mdp, seed=None):
 
 
 seeds = [9, 44, 404, 240, 259, 141, 371, 794, 41, 507, 819, 959, 829, 558, 638, 127, 672, 4, 635, 687]
+seeds.extend([101,102,103,104,105,106,107,108,109,110])
+seeds.extend([111,112,113,114,115,116,117,118,119,120])
+seeds.extend([131,132,133,134,135,136,137,138,139,140])
 seeds = seeds[:n_runs]
 
 # On each run use a random mdp from the ones created
 mdps = np.random.choice(mdps, len(seeds))
+
+if load_results:
+    old_results = utils.load_object(file_name)
+    skip = len(old_results)
+    mdps = mdps[skip:]
+    seeds = seeds[skip:]
+
+print("Seeds:", seeds)
 
 if n_jobs == 1:
     results = [run(mdp,seed) for (mdp,seed) in zip(mdps,seeds)]
 elif n_jobs > 1:
     results = Parallel(n_jobs=n_jobs)(delayed(run)(mdp,seed) for (mdp,seed) in zip(mdps,seeds))
 
-utils.save_object(results, file_path + file_name)
+if load_results:
+    old_results.extend(results)
+    utils.save_object(old_results,file_name)
+else:
+    utils.save_object(results,file_name)
