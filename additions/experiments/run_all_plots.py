55052cef6167b5029c7f498475fda33d1b61ebeb
Andrea Soprani <andrea.soprani96@gmail.com>
2020-06-21 18:57:35 +0200

    mountaincar linear pc=1 lambda test

--- additions/experiments/run_all_plots.py
+++ additions/experiments/run_all_plots.py
@@ -31,7 +31,8 @@ results_paths = [
     "results/three-room-gw/polynomial/lambda_test/pc=1/ --lambda_test=True",
     "results/three-room-gw/polynomial/lambda_test/pc=3/ --lambda_test=True",
     "results/three-room-gw/sin/lambda_test/pc=1/ --lambda_test=True",
-    "results/three-room-gw/sin/lambda_test/pc=3/ --lambda_test=True"
+    "results/three-room-gw/sin/lambda_test/pc=3/ --lambda_test=True",
+    "results/mountaincar/linear/lambda_test/pc=1/ --lambda_test=True"
 ]
 
 show = False
